6c7844a66b41e5a5b554475ea43fbf63ba4664a6
Aurora <aurawra@outlook.com>
2025-09-09 18:53:27 -0500

    created database migration and models,, udpated root route, created scaffolding for root route, and modified gem file

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
+# root = initial page for application (i.e. "/") --> forcing redirect to movies page
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :movies
+  root :to => redirect('/movies#index') # bad practice to have same page twice
+  # can redirect the URL to the movies#index, or you could write, resources :movies, except: [:index]
 end
