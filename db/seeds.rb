1a4e5dabf4369ddb6b605e159288185b506ab593
Aurora <aurawra@outlook.com>
2025-09-10 10:22:21 -0500

    rearranged movies into table format

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,8 @@
 more_movies = [
   {:title => 'My Neighbor Totoro', :rating => 'G',
     :release_date => '16-Apr-1988'},
+  {:title => 'Crazy Rich Asians', :rating => 'PG-13',
+    :release_date => '15-Aug-2018'},
   {:title => 'Green Book', :rating => 'PG-13',
     :release_date => '16-Nov-2018'},
   {:title => 'Parasite', :rating => 'R',
@@ -19,7 +21,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Everything Everywhere All at Once', :rating => 'R',
+    :release_date => '8-Apr-2022'},
+  {:title => 'KPop Demon Hunters', :rating => 'PG',
+    :release_date => '23-Aug-2025'},
+  {:title => 'Demon Slayer: Kimetsu no Yaiba Infinity Castle', :rating => 'R',
+    :release_date => '12-Sept-2025'},
 ]
 
 more_movies.each do |movie_attrs|
